6b7e8c37080e5f1be63fdd54110b132b3eb1a9cf
Andrey Sitnikov <84133942+ruandrinho@users.noreply.github.com>
2022-09-12 16:12:30 +0500

    Clean up

--- main.py
+++ main.py
@@ -46,22 +46,17 @@ if __name__ == '__main__':
             response.raise_for_status()
             devman_reviews = response.json()
             if devman_reviews['status'] == 'timeout':
-                timestamp_to_request =\
-                    devman_reviews['timestamp_to_request']
+                timestamp_to_request = devman_reviews['timestamp_to_request']
             elif devman_reviews['status'] == 'found':
                 for attempt in devman_reviews['new_attempts']:
-                    message = f'У вас проверили работу '\
-                              f'«{attempt["lesson_title"]}»\n\n'
+                    message = f'У вас проверили работу «{attempt["lesson_title"]}»\n\n'
                     if attempt['is_negative']:
                         message += 'К сожалению, в работе нашлись ошибки\n\n'
                     else:
-                        message += 'Преподавателю всё понравилось, '\
-                                   'можно приступать к следующему уроку!\n\n'
+                        message += 'Преподавателю всё понравилось, можно приступать к следующему уроку!\n\n'
                     message += attempt['lesson_url']
-                # bot.send_message(chat_id=chat_id, text=message)
                 logger.warning(message)
-                timestamp_to_request =\
-                    devman_reviews['last_attempt_timestamp']
+                timestamp_to_request = devman_reviews['last_attempt_timestamp']
         except requests.exceptions.ReadTimeout:
             pass
         except requests.exceptions.ConnectionError:
